f240f80ed605614167d82f23d73f293cca9be6a1
rookiepeng <rookiepeng@gmail.com>
2019-05-19 14:54:04 -0400

    add windows

--- Linear_Array.py
+++ Linear_Array.py
@@ -40,25 +40,28 @@
 
 import numpy as np
 from antarray import Antenna_Array
+from scipy.signal import chebwin, hamming, hann
+from taylor_win import taylor
 
 
 class Linear_Array(Antenna_Array):
     def __init__(self, size, spacing=0.5):
         self.size = size
         self.spacing = spacing
+        self.window_dict = {
+            'square': self.square_win,
+            'chebyshev': self.chebyshev_win,
+            'taylor': self.taylor_win,
+            'hamming': self.hamming_win,
+            'hanning': self.hann_win
+        }
         Antenna_Array.__init__(self, x=np.arange(
             0, size, 1)*spacing)
 
-    def get_pattern(self, theta, beam_loc=0, window='square'):
-        # window_type = linear_array_config['window_type_idx']
-        # window_sll = linear_array_config['window_sll']
-        # window_nbar = linear_array_config['window_nbar']
-
+    def get_pattern(self, theta, beam_loc=0, window='square', sll=-60, nbar=4):
         weight = np.exp(-1j * 2 * np.pi * self.x * np.sin(
             beam_loc / 180 * np.pi))
-        # * window_dict[
-        # window_type](array_size, window_sll,
-        #            window_nbar)
+        * self.window_dict[window](self.size, sll, nbar)
 
         weight = weight / np.sum(np.abs(weight))
 
@@ -75,3 +78,18 @@ class Linear_Array(Antenna_Array):
         keys = ['size', 'spacing']
         self.__dict__.update((k, v) for k, v in kwargs.items() if k in keys)
         self.__init__(self.size, self.spacing)
+
+    def square_win(self, *args, **kwargs):
+        return 1
+
+    def chebyshev_win(self, array_size, sll, *args, **kwargs):
+        return chebwin(array_size, at=sll)
+
+    def taylor_win(self, array_size, sll, nbar):
+        return taylor(array_size, nbar, -sll)
+
+    def hamming_win(self, array_size, *args, **kwargs):
+        return hamming(array_size)
+
+    def hann_win(self, array_size, *args, **kwargs):
+        return hann(array_size)
