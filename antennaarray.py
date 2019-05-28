2c8f805e79c7aabd31c3f009158bb7d410841c9a
rookiepeng <rookiepeng@gmail.com>
2019-05-27 21:07:25 -0400

    put weighting in to calculation

--- antennaarray.py
+++ antennaarray.py
@@ -79,14 +79,30 @@ class AntennaArray:
 
         Parameters
         ----------
-        theta : 1-D array
-            Angles for calculation (deg)
+        u : 1-D array
+            If `polar` is `True': angles for calculation (deg)
+            If `polar` is `False': directional cosines,
+            $u=sin(\theta)cos(\phi)$
+        v : 1-D array
+            If `polar` is `True': angles for calculation (deg)
+            If `polar` is `False': directional cosines,
+            $v=sin(\theta)sin(\phi)$
+        weight : 1-D array (complex), optional
+            Weightings for array elements (default is None)
+        polar : boolean
+            Indicate the types of `u` and `v`
 
         Returns
         -------
-        AF : 1-D array
-            Array pattern in decibels (dB)
+        array_factor : 1-D array
+            Array pattern in linear scale
+        u : 1-D array
+            Directional cosines
+        v : 1-D array
+            Directional cosines
         """
+
+        size = len(self.x)
 
         if polar:
             theta_grid, phi_grid = np.meshgrid(u, v)
@@ -97,12 +113,15 @@ class AntennaArray:
         else:
             u_grid, v_grid = np.meshgrid(u, v)
 
-        size = len(self.x)
+        if weight is None:
+            weight = np.ones(size)
+
         AF = np.zeros(np.shape(u_grid), dtype=complex)
 
         for idx in range(0, size):
-            AF = AF + np.exp(1j * 2 * np.pi *
-                             (self.x[idx]*u_grid + self.y[idx]*v_grid))
+            AF = AF + \
+                np.exp(1j * 2 * np.pi *
+                       (self.x[idx]*u_grid + self.y[idx]*v_grid)) * weight[idx]
 
         return {'u': u_grid,
                 'v': v_grid,
