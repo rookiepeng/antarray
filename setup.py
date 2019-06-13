47807c7cfc86b0f89aadba331460162c0fabeffb
Zach Peng <rookie.dev@gmail.com>
2019-06-13 10:23:24 -0400

    correct module path

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 ext_modules = [
-    Extension('antennaarray', ['antennaarray.py']),
-    Extension('lineararray', ['lineararray.py']),
-    Extension('rectarray', ['rectarray.py']),
+    Extension('antennaarray', ['./antarray/antennaarray.py']),
+    Extension('lineararray', ['./antarray/lineararray.py']),
+    Extension('rectarray', ['./antarray/rectarray.py']),
 ]
 setup(name='antarray',
       cmdclass={'build_ext': build_ext},
