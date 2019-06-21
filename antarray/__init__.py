2c99bb8a39429db2870b12c012bef8d93ef23550
Zach Peng <rookie.dev@gmail.com>
2019-06-21 16:48:58 -0400

    add version after import

--- antarray/__init__.py
+++ antarray/__init__.py
@@ -30,10 +30,10 @@
 
 """
 
-__version__ = '1.0.5'
 import numpy
 from scipy import signal
 
 from .antennaarray import AntennaArray
 from .lineararray import LinearArray
 from .rectarray import RectArray
+__version__ = '1.0.5'
