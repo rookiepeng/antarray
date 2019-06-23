e01c780676c655b1c41d55d7c16dd64333d07c60
rookiepeng <rookiepeng@gmail.com>
2019-06-22 22:50:20 -0400

    unit test for linear array

--- tests/test_lineararray.py
+++ tests/test_lineararray.py
@@ -7,6 +7,12 @@ def test_lineararray():
     assert np.array_equal(lin_array.x, np.arange(0, 16, 1)*0.5)
     assert np.array_equal(lin_array.y, np.zeros(16))
 
-    lin_array = LinearArray(size=32, spacing=1)
+    lin_array.update_parameters(size=32, spacing=1)
     assert np.array_equal(lin_array.x, np.arange(0, 32, 1)*1)
     assert np.array_equal(lin_array.y, np.zeros(32))
+
+    theta = np.arange(-90, 90, 1)
+    lin_array.update_parameters(spacing=0.5)
+    data = lin_array.get_pattern(theta=theta, beam_loc=10)
+    assert np.max(np.abs(data['array_factor'])) == 1
+    assert theta[np.argmax(np.abs(data['array_factor']))] == 10
